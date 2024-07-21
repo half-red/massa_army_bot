e37b0bfc9cab4f01776fdf93ce30d5cb4cf80452
half-red <half-red@half-red.net>
2024-07-21 03:33:40 +0200

    Working depuplicator

--- src/app/app.py
+++ src/app/app.py
@@ -1,18 +1,27 @@
 import asyncio
 import os
+import re
 from datetime import datetime
 from datetime import timezone as tz
 from functools import partial
+from html import escape
 from pathlib import Path
+from pprint import pformat
 from traceback import format_exc
 
+from app.db import Database
 from telethon import events
 from telethon import TelegramClient
+from telethon import utils
+from telethon.sessions.sqlite import sqlite3
 from telethon.tl.custom.message import Message
 
 datadir = Path("data")
 sessions = datadir / "sessions"
 sessions.mkdir(exist_ok=True, parents=True)
+
+dbfile = datadir / "twitter_posts.sqlite3"
+db = Database()
 
 class HalfRed:
     tg: TelegramClient
@@ -39,18 +48,19 @@ class HalfRed:
         msg = "%s\n%s" % (fdate, msg)
         if file:
             msg = "%s\nfile:%s" % (msg, file)
-        print(msg)
+        # print(msg)
         return await self.tg.send_message(
             self.log_channel, msg,
             file=file,  # type: ignore
             parse_mode="html", *args, **kwargs)
 
-    def run(self, startup=None):
+    def run(self, startup=None, *startup_args, **startup_kwargs):
         with self.tg:
             loop = self.tg.loop
             loop.run_until_complete(self.startup())
             if startup is not None:
-                loop.run_until_complete(startup())
+                loop.run_until_complete(
+                    startup(*startup_args, **startup_kwargs))
             self.tg.run_until_disconnected()
 
     def cmd(self, func=None, /, **params):
@@ -69,6 +79,99 @@ class HalfRed:
                 raise
             return e
 
+pf = partial(pformat, width=30, indent=2, sort_dicts=False)
+
+tw_username: str
+tw_post_id: int
+tg_msg_by: int
+tg_msg_at: int
+tg_msg_chat: int
+tg_msg_id: int
+tg_msg_topic: int | None
+url: str
+
+async def init_db(dbfile, delete_before=False):
+    await db.connect(dbfile, debug=False, delete_before=delete_before)
+    async with db.tx() as tx:
+        await tx.execute(
+            "CREATE TABLE IF NOT EXISTS tw_posts ("
+            "tw_username TEXT NOT NULL, "
+            "tw_post_id INTEGER NOT NULL, "
+            "tg_msg_by INTEGER NOT NULL, "
+            "tg_msg_at INTEGER NOT NULL, "
+            "tg_msg_chat INTEGER NOT NULL, "
+            "tg_msg_id INTEGER NOT NULL, "
+            "tg_msg_topic INTEGER, "
+            "url TEXT)"
+        )
+        await tx.execute(
+            "CREATE UNIQUE INDEX idx_tw_posts "
+            "ON tw_posts (tw_username, tw_post_id);")
+
+chat_types = {}
+async def get_chat_type(event: events.NewMessage.Event):
+    msg: Message = event.message
+    c_id = event.chat_id
+    if c_id in chat_types:
+        return chat_types[c_id]
+    chat = await event.get_chat()
+    assert chat
+    if msg.is_private:
+        chat_types[c_id] = "private"
+    elif chat.forum:
+        chat_types[c_id] = "topics"
+    else:
+        chat_types[c_id] = "group"
+    print(f"{chat_types[c_id]=}")
+    return chat_types[c_id]
+
+async def get_topic(event: events.NewMessage.Event):
+    chat_type = await get_chat_type(event)
+    print(f"{chat_type=}")
+    if chat_type != "topics":
+        return None
+    msg: Message = event.message
+    if not msg.reply_to:
+        return "General"
+
+async def extract_info(event: events.NewMessage.Event):
+    tg_msg_by = event.sender_id
+    tg_msg_at = int((event.date or datetime.now(tz=tz.utc)).timestamp())
+    tg_msg_chat = event.chat_id
+    real_chat_id, _ = utils.resolve_id(tg_msg_chat)
+    tg_msg_topic = await get_topic(event)
+    tg_msg_id = event.id
+    url = None
+    if not event.is_private:
+        url = f"https://t.me/c/{real_chat_id}/{tg_msg_id}"
+    return (tg_msg_by, tg_msg_at,
+            tg_msg_chat, tg_msg_topic, tg_msg_id,
+            url)
+
+twitter_url_pattern = re.compile(
+    "<a href=\"(?P<href>"
+    r"(?:https?://)?(?:x|twitter).com?/"
+    r"(?P<tw_username>[^/]*)/status/"
+    r"(?P<tw_post_id>\d+)([/?]?\S*)?"
+    ")\">(?:.*?(?=</a>))</a>",
+    re.MULTILINE | re.DOTALL | re.IGNORECASE
+)
+
+print(f"{twitter_url_pattern=}")
+
+parse_mode = utils.sanitize_parse_mode("html")
+def to_html(event):
+    return parse_mode.unparse(event.raw_text, event.entities)
+
+def from_html(text):
+    return parse_mode.parse(text)
+
+inc_link_template = '<a href="%%s">%s</a>'
+dup_template = inc_link_template % 'Marked as duplicate'
+link_template = inc_link_template % '%s'
+tw_url_template = "https://x.com/%s/status/%s"
+quote_template = "<blockquote>%s</blockquote>"
+
 def main():
     hr = HalfRed(username=os.environ["TG_BOT_USERNAME"],
                  api_id=os.environ["TG_API_ID"],
@@ -76,10 +179,92 @@ def main():
                  bot_token=os.environ["TG_BOT_TOKEN"],
                  log_channel=os.environ["TG_LOG_CHANNEL"])
 
-    @hr.cmd(pattern=r"")
-    def grab_tweet(event):
-        print(event)
-    hr.run()
+    @hr.cmd
+    async def _(event: events.NewMessage.Event):
+        text = to_html(event)
+        if not text:
+            return
+        tg_msg_by, tg_msg_at, tg_msg_chat, tg_msg_id, tg_msg_topic, url = await extract_info(event)
+        response_ok = []
+        response_duplicate = []
+        has_duplicates = False
+        last_end, start, end = 0, 0, 0
+        for m in twitter_url_pattern.finditer(text):
+            if not m:
+                continue
+            start, end = m.span()
+            match = text[start:end]
+            g = m.groupdict()
+            tw_username = g["tw_username"]
+            tw_post_id = g["tw_post_id"]
+            try:
+                async with db.tx() as tx:
+                    await tx.execute(
+                        "INSERT INTO tw_posts ( "
+                        "tw_username, "
+                        "tw_post_id, "
+                        "tg_msg_by, "
+                        "tg_msg_at, "
+                        "tg_msg_chat, "
+                        "tg_msg_id, "
+                        "tg_msg_topic, "
+                        "url) "
+                        "VALUES ("
+                        "?, ?, ?, ?, ?, ?, ?, ?"
+                        ");",
+                        (tw_username, tw_post_id,
+                         tg_msg_by, tg_msg_at,
+                         tg_msg_chat, tg_msg_topic, tg_msg_id,
+                         url))
+                response_ok.append(text[last_end:end])
+            except sqlite3.IntegrityError:
+                has_duplicates = True
+                async with db.tx() as tx:
+                    cur = await tx.execute(
+                        "SELECT * "
+                        "FROM tw_posts "
+                        "WHERE tw_username = ? "
+                        "AND tw_post_id = ?",
+                        (tw_username, tw_post_id)
+                    )
+                    row = await cur.fetchone()
+                    assert row
+                    (tw_username, tw_post_id,
+                     tg_msg_by, tg_msg_at,
+                     tg_msg_chat, tg_msg_id, tg_msg_topic,
+                     url) = row
+                if event.is_private:
+                    response_ok.append(
+                        text[last_end:start]
+                        + dup_template % escape(
+                            tw_url_template % (
+                                tw_username,
+                                tw_post_id)))
+                else:
+                    response_ok.append(
+                        text[last_end:start]
+                        + dup_template % escape(url)
+                    )
+                response_duplicate.append(
+                    "%s\n" % (
+                        match))
+            last_end = end
+        if has_duplicates:
+            if response_ok:
+                await event.reply(
+                    "".join((*response_ok, text[end:])),
+                    parse_mode="html")
+            duplicates = await event.reply(
+                "\n\n".join([
+                    "Duplicate posts:",
+                    *response_duplicate,
+                    "This message will self-destruct in 10s"
+                ]),
+                parse_mode="html")
+            await hr.tryf(event.delete)
+            await asyncio.sleep(10)
+            await hr.tryf(duplicates.delete)
+    hr.run(startup=init_db, dbfile=dbfile, delete_before=True)
 
 if __name__ == "__main__":
     main()
---
+++ src/app/db.py
@@ -0,0 +1,121 @@
+import asyncio
+import textwrap as tw
+import traceback as tb
+from contextlib import asynccontextmanager
+from functools import partial
+from functools import wraps
+from itertools import chain
+from pathlib import Path
+
+import aiosqlite
+from aiosqlite.context import contextmanager as aioscontextmanager
+
+def repr_arg(arg):
+    if callable(arg):
+        return arg.__qualname__
+    return repr(arg)
+
+def repr_args(args, kwargs):
+    return ", ".join(chain(
+        (f"{repr_arg(a)}" for a in args),
+        (f"{k}={repr_arg(v)}" for k, v in kwargs.items())))
+
+def repr_call(func, *args, _indent='', **kwargs):
+    return tw.indent(f"{func.__qualname__}({repr_args(args, kwargs)})", _indent)
+def debuggable(func, proxy_root, func_is_dispatcher=False, indent=''):
+    def wrapper(m, *a, debug=None, showresult=False, **kw):
+        debug = proxy_root.debug if debug is None else debug
+        if debug:
+            if func_is_dispatcher:
+                print(repr_call(m, *a, _indent=indent, **kw))
+            else:
+                print(repr_call(func, m, *a, _indent=indent, **kw))
+        res = func(m, *a, **kw)
+        if debug and showresult:
+            print(tw.indent(f"->{repr_arg(res)}", indent))
+        return res
+    return wrapper
+
+class AioSqliteError(Exception):
+    ...
+
+@asynccontextmanager
+async def FakeLock():
+    yield
+
+class Database:
+    def __init__(self) -> None:
+        self._lock = asyncio.Lock()
+        self._no_lock = FakeLock
+        self._con: aiosqlite.Connection = None  # type: ignore
+
+    async def connect(self, db_file, debug=False, delete_before=False):
+        assert self._con is None, "Database already connected"
+        if debug:
+            print(
+                f"Database.connect({str(db_file)!r}, {debug=!r}, {delete_before=!r})")
+        db_file = Path(db_file)
+        wal_file = Path(str(db_file) + "-wal")
+        shm_file = Path(str(db_file) + "-shm")
+        if delete_before:
+            db_file.unlink(missing_ok=True)
+            wal_file.unlink(missing_ok=True)
+            shm_file.unlink(missing_ok=True)
+            print("Deleted db")
+        self._con = await aiosqlite.connect(db_file, isolation_level=None)
+        self.debug = debug
+        # inject debuggable wrapper to the self._con execute* methods
+        _debuggable = partial(debuggable, proxy_root=self,
+                              func_is_dispatcher=True, indent="| ")
+        self._con._execute = _debuggable(self._con._execute)  # type: ignore
+        # executemany autocommits before execting, interferes with self.tx()
+        self._con.executemany = None  # type: ignore
+        # patches self._con.cursor to produce a Cursor with debuggable execute* methods
+
+        class Cursor(aiosqlite.Cursor):
+            @classmethod
+            def __prepare__(cls, name, bases, **kwargs):
+                sup = super().__prepare__(name, bases, **kwargs)
+                return {'_execute': _debuggable(sup['execute'])}
+
+            def __init__(self, *a, **kw):
+                super().__init__(*a, **kw)
+                self.executemany = None  # type: ignore
+
+        @aioscontextmanager
+        @wraps(self._con.cursor)
+        async def customcursor(self, *a, **kw):
+            return Cursor(*a, **kw)
+        self._con.cursor = customcursor  # type: ignore
+        if debug:
+            print("Configuring database")
+        async with self._lock:
+            async with self._con.execute("pragma journal_mode=wal") as cur:
+                row = await cur.fetchone()
+                assert row is not None
+                mode, = row
+                assert mode == 'wal', "Could not enable WAL mode..."
+            await self._con.commit()
+        if debug:
+            print("Configured database")
+
+    @asynccontextmanager
+    async def tx(self, debug=None,
+                 lock=False, print_tb=False):
+        debug = self.debug if debug == None else debug
+        async with self._lock if lock else self._no_lock():
+            try:
+                if debug:
+                    print("Tx started")
+                await self._con.execute("begin")
+                yield self._con
+                await self._con.commit()
+                if debug:
+                    print("Tx committed")
+            except Exception:
+                await self._con.rollback()
+                if print_tb:
+                    (tb.format_exc())
+                if debug:
+                    print("Tx reverted")
+                raise
